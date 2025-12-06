86279f23f1e6b4ee84a3c046e2306f20e6838d9a
Anushka Majumdar <anushka.dpsrpk@gmail.com>
2025-12-06 18:02:45 +0000

    updated init.py

--- pinn_module/__init__.py
+++ pinn_module/__init__.py
@@ -1,3 +1,3 @@
 from .simulator import PINNSimulator
-from .validation import validate_against_cfd, load_baseline_case, physics_residual_check
-
+from .validation import validate_against_cfd, load_baseline_case
+from .model import PINN
